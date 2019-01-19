1de752fb3f8a657a96ef30e1d08caa1a685331af
Lucas <lucasferguson16@gmail.com>
2019-01-19 13:40:08 -0600

    Changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,7 +25,7 @@ import frc.robot.subsystems.ExampleSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+  public static ExampleSubsystem m_subsystAem = new ExampleSubsystem();
   public static OI m_oi;
 
   Command m_autonomousCommand;
