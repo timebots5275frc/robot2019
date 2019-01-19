bccce2a2f755b3441f259d610cf177a27e97e18c
Lucas <lucasferguson16@gmail.com>
2019-01-19 14:52:34 -0600

    Fixing Stash

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -19,4 +19,13 @@ public class OI {
   
 
 
+  //// CREATING BUTTONS
+  // One type of button is a joystick button which is any button on a
+  //// joystick.
+  // You create one by telling it which joystick it's on and which button
+  // number it is.
+  // Joystick stick = new Joystick(port);
+  // Button button = new JoystickButton(stick, buttonNumber);
+
+
 }
